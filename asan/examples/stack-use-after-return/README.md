9e0a73f720214c7dfb263a6775bab4db850806fa
dengkai01 <dengkai01@corp.netease.com>
2022-03-24 18:37:10 +0800

    更新内容

---
+++ asan/examples/stack-use-after-return/README.md
@@ -0,0 +1,6 @@
+
+```
+ASAN_OPTIONS=detect_stack_use_after_return=1 ./uar_c
+```
+
+目前cpp程序不会触发。
---
+++ asan/examples/stack-use-after-return/uar_c.c
@@ -0,0 +1,16 @@
+// example1.cpp
+// stack-use-after-return error
+char *x;
+
+void foo()
+{
+    char stack_buffer[42];
+    x = &stack_buffer[13];
+}
+
+int main()
+{
+    foo();
+    *x = 42; // Boom!
+    return 0;
+}
