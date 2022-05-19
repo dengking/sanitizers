8bd6da76d00cfba729e2950449aeab4ff9255439
dengkai01 <dengkai01@corp.netease.com>
2022-05-19 11:26:46 +0800

    更新内容

---
+++ asan/examples/asan-default-options/halt_on_error_log_path.cc
@@ -0,0 +1,34 @@
+// https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow
+#include <vector>
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    const char *__asan_default_options()
+    {
+        return "halt_on_error=true:log_path=./asan.log";
+    }
+#ifdef __cplusplus
+}
+#endif
+
+typedef long T;
+int main()
+{
+    std::vector<T> v;
+    v.push_back(0);
+    v.push_back(1);
+    v.push_back(2);
+    // This assertion fails in windows, for cross platform, it should be commented out.
+    // assert(v.capacity() >= 4);
+    assert(v.size() == 3);
+    T *p = &v[0];
+    // Here the memory is accessed inside a heap-allocated buffer
+    // but outside of the region `[v.begin(), v.end())`.
+    return p[3]; // OOPS.
+                 // v[3] could be detected by simple checks in std::vector.
+                 // *(v.begin()+3) could be detected by a mighty debug iterator
+                 // (&v[0])[3] can only be detected with AddressSanitizer or similar.
+}
