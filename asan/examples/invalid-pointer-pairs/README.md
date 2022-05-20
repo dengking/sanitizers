f4bc4a1be4094dfe3476c789a33d9beec6c3e0fc
dengkai01 <dengkai01@corp.netease.com>
2022-05-20 10:54:39 +0800

    更新内容

---
+++ asan/examples/invalid-pointer-pairs/README.md
@@ -0,0 +1,10 @@
+
+
+https://github.com/llvm/llvm-project/blob/main/compiler-rt/test/asan/TestCases/invalid-pointer-pairs-compare-errors.cpp
+
+
+
+https://github.com/llvm/llvm-project/blob/main/compiler-rt/test/asan/TestCases/invalid-pointer-pairs-subtract-errors.cpp
+
+
+https://kristerw.blogspot.com/2018/06/
---
+++ asan/examples/invalid-pointer-pairs/pointer-subtraction.cc
@@ -0,0 +1,27 @@
+// https://kristerw.blogspot.com/2018/06/
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    const char *__asan_default_options()
+    {
+        return "detect_invalid_pointer_pairs=2";
+    }
+#ifdef __cplusplus
+}
+#endif
+
+int main(void)
+{
+    char *p = (char *)malloc(42);
+    char *q = (char *)malloc(42);
+
+    int tmp = p - q; // Error
+
+    free(p);
+    free(q);
+
+    return tmp;
+}
