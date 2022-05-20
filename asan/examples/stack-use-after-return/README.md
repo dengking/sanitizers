333035a8233736bf3fa2fe380fd13d3ad661e70e
dengkai01 <dengkai01@corp.netease.com>
2022-05-20 16:04:53 +0800

    更新内容

--- asan/examples/stack-use-after-return/README.md
+++ asan/examples/stack-use-after-return/README.md
@@ -9,3 +9,7 @@ ASAN_OPTIONS=detect_stack_use_after_return=1 ./uar_c
 ```
 ASAN_OPTIONS=detect_stack_use_after_return=1 ./uar_cpp 1
 ```
+
+windows: 
+
+不支持
--- asan/examples/stack-use-after-return/uar_c.c
+++ asan/examples/stack-use-after-return/uar_c.c
@@ -1,6 +1,21 @@
 // https://docs.microsoft.com/en-us/cpp/sanitizers/error-stack-use-after-return?view=msvc-170
 // example1.cpp
 // stack-use-after-return error
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    const char* __asan_default_options()
+    {
+        return "detect_stack_use_after_return=1";
+    }
+#ifdef __cplusplus
+}
+#endif
+
+
 char *x;
 
 void foo()
