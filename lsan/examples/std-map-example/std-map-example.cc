30cf7cc0db2416e548ff2f383dbf4243d7c977a9
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 15:39:22 +0800

    更新内容

---
+++ lsan/examples/std-map-example/std-map-example.cc
@@ -0,0 +1,9 @@
+#include <map>
+#include <unordered_map>
+std::map<int, bool> *m;
+std::unordered_map<int, bool> *m2;
+int main(int argc, char **argv)
+{
+    m = new std::map<int, bool>;
+    m2 = new std::unordered_map<int, bool>;
+}
