b397e0ccad9fde04f6dc1333ba3d7cd06f2ad76f
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 17:41:00 +0800

    Update std-vector-leak-example4.cc

--- lsan/examples/std-vector-leak-example4/std-vector-leak-example4.cc
+++ lsan/examples/std-vector-leak-example4/std-vector-leak-example4.cc
@@ -9,11 +9,13 @@ int main(int argc, char **argv)
     global->push_back(new int[20]);
     global->push_back(new int[30]);
     global->push_back(new int[40]);
+    global = 0;
     {
         auto *local = new std::vector<int *>;
         local->push_back(new int[10]);
         local->push_back(new int[20]);
         local->push_back(new int[30]);
         local->push_back(new int[40]);
+        local = 0;
     }
 }
