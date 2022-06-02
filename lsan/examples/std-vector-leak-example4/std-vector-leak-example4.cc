b2e395c97e9fff6a3b034ae7a7e25c7fc1dfca0c
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 17:38:33 +0800

    Update std-vector-leak-example4.cc

--- lsan/examples/std-vector-leak-example4/std-vector-leak-example4.cc
+++ lsan/examples/std-vector-leak-example4/std-vector-leak-example4.cc
@@ -1,6 +1,19 @@
 // https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow
 #include <vector>
+std::vector<int *> *global;
 int main(int argc, char **argv)
 {
+    global = new std::vector<int *>;
 
+    global->push_back(new int[10]);
+    global->push_back(new int[20]);
+    global->push_back(new int[30]);
+    global->push_back(new int[40]);
+    {
+        auto *local = new std::vector<int *>;
+        local->push_back(new int[10]);
+        local->push_back(new int[20]);
+        local->push_back(new int[30]);
+        local->push_back(new int[40]);
+    }
 }
