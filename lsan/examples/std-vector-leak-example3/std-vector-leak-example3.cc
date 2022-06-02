2cca3a2855537f683089fd18901c8fc38879d8be
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 14:14:41 +0800

    添加新例子

---
+++ lsan/examples/std-vector-leak-example3/std-vector-leak-example3.cc
@@ -0,0 +1,11 @@
+// https://github.com/google/sanitizers/wiki/AddressSanitizerContainerOverflow
+#include <vector>
+std::vector<int *> v;
+int main(int argc, char **argv)
+{
+    v.push_back(new int[10]);
+    v.push_back(new int[20]);
+    v.push_back(new int[30]);
+    v.push_back(new int[40]);
+    v.pop_back(); // The last element leaks now.
+}
