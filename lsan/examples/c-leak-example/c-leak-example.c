a37c2686094a43bad5344e038dcc87cf4497d44f
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 17:47:57 +0800

    Update c-leak-example.c

--- lsan/examples/c-leak-example/c-leak-example.c
+++ lsan/examples/c-leak-example/c-leak-example.c
@@ -1,11 +1,22 @@
 // https://github.com/google/sanitizers/wiki/AddressSanitizerLeakSanitizer
 
 #include <stdlib.h>
+void FooBar()
+{
+    malloc(7);
+}
+
+void Baz()
+{
+    malloc(5);
+}
 
 void *global;
 
 int main()
 {
+    FooBar();
+    Baz();
     global = malloc(7);
     global = 0; // The memory is leaked here.
     return 0;
