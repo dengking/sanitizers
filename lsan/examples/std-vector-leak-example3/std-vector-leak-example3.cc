cb1403b0ce260011a89b1ebf459ce006b4c88a37
dengkai01 <dengkai01@corp.netease.com>
2022-06-02 17:19:37 +0800

    更新内容

--- lsan/examples/std-vector-leak-example3/std-vector-leak-example3.cc
+++ lsan/examples/std-vector-leak-example3/std-vector-leak-example3.cc
@@ -8,4 +8,12 @@ int main(int argc, char **argv)
     global.push_back(new int[30]);
     global.push_back(new int[40]);
     global.pop_back(); // The last element leaks now.
+    {
+        std::vector<int *> local;
+        local.push_back(new int[10]);
+        local.push_back(new int[20]);
+        local.push_back(new int[30]);
+        local.push_back(new int[40]);
+        local.pop_back(); // The last element leaks now.
+    }
 }
