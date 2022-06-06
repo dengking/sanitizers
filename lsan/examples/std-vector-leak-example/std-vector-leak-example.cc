1cbf9833d9ff8948bc7c938cb6104c34c209b61b
dengkai01 <dengkai01@corp.netease.com>
2022-06-06 16:36:03 +0800

    Update std-vector-leak-example.cc

--- lsan/examples/std-vector-leak-example/std-vector-leak-example.cc
+++ lsan/examples/std-vector-leak-example/std-vector-leak-example.cc
@@ -3,9 +3,9 @@ std::vector<int *> *global;
 int main(int argc, char **argv)
 {
     global = new std::vector<int *>;
-    global = 0;
+    global = 0; // leak
     {
         auto *local = new std::vector<int *>;
-    }
+    } // leak
     return 0;
 }
